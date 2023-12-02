e967c7b409d0944c5ca9799680ee4e8bbe8cb5fa
Taranovskaya <s5198653@yandex.ru>
2023-12-02 22:37:22 +0300

    Добавляет разметку, стилизацию и js блока header

--- source/js/main.js
+++ source/js/main.js
@@ -1,23 +1,22 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import './modules/burger-menu/locomotive-scroll';
+import {Burger} from './modules/burger-menu/burger';
+import {StickyHeader} from './modules/burger-menu/sticky-header';
+import {initLocomotiveScroll} from './modules/burger-menu/init-locomotive-scroll';
 
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
-
-  // Utils
-  // ---------------------------------
-
+  initLocomotiveScroll();
   iosVhFix();
 
-  // Modules
-  // ---------------------------------
-
-  // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
-  // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
+    const burger = new Burger();
+    burger.init();
+    const stickyHeader = new StickyHeader();
+    stickyHeader.init();
+
     const form = new Form();
     window.form = form;
     form.init();
