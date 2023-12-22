9932e36ef70f3859d611e70f54a4687228edf1c5
Taranovskaya <s5198653@yandex.ru>
2023-12-22 23:43:26 +0300

    Вносит правки в блок reviews после self-test

--- source/js/modules/reviews-swiper.js
+++ source/js/modules/reviews-swiper.js
@@ -2,21 +2,23 @@ const initReviewsSwiper = () =>
   new window.Swiper('[data-swiper="reviews-swiper"]', {
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 'auto',
-    spaceBetween: 120,
     navigation: {
       nextEl: '[data-button="reviews-next"]',
       prevEl: '[data-button="reviews-prev"]',
     },
     breakpoints: {
       1200: {
+        slidesPerView: 'auto',
+        spaceBetween: 120,
         allowTouchMove: false,
       },
       768: {
+        slidesPerView: 'auto',
         spaceBetween: 30,
         allowTouchMove: true,
       },
-      320: {
+      350: {
+        slidesPerView: 1,
         spaceBetween: 0,
         allowTouchMove: true,
       },
